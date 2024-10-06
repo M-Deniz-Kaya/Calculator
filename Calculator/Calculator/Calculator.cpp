ca8387c45ed6a0d79545e5f45b349b6da6ec3902
M-Deniz-Kaya <mehmetdenizkaya2006@gmail.com>
2024-10-06 11:58:41 +0300

    Calculator
    Calculator for whole numbers and decimal numbers.

---
+++ Calculator/Calculator/Calculator.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <limits>
+
+int main() {
+    float a, b; // We use float to hold both int and float values
+    char transaction;
+
+    while (true) {
+        std::cout << "First number (enter 'q' to exit): ";
+        if (!(std::cin >> a)) {
+            std::cin.clear(); // Resetting error status
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Clearing the input buffer
+            std::cout << "exiting" << std::endl;
+            break;
+        }
+
+        std::cout << "second number: ";
+        std::cin >> b;
+
+        std::cout << "Enter transaction (+, -, *, /): ";
+        std::cin >> transaction;
+
+        switch (transaction) {
+        case '+':
+            std::cout << "response : " << a + b << std::endl;
+            break;
+
+        case '-':
+            std::cout << "response : " << a - b << std::endl;
+            break;
+
+        case '*':
+            std::cout << "response : " << a * b << std::endl;
+            break;
+
+        case '/':
+            if (b != 0) {
+                std::cout << "response : " << a / b << std::endl;
+            }
+            else {
+                std::cout << "error: division by zero!" << std::endl;
+            }
+            break;
+
+        default:
+            std::cout << "unknown transaction." << std::endl;
+        }
+
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
