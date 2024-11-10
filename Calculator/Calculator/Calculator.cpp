07d97c4988f7de19c53aa50e4f335e4d66a32eb6
M-Deniz-Kaya <mehmetdenizkaya2006@gmail.com>
2024-11-10 11:07:23 +0300

    Update

--- Calculator/Calculator/Calculator.cpp
+++ Calculator/Calculator/Calculator.cpp
@@ -2,52 +2,92 @@
 #include <limits>
 
 int main() {
-    float a, b; // We use float to hold both int and float values
-    char transaction;
+
 
     while (true) {
+
+        float a, b; // We use float to hold both int and float values
+        char transaction;
+        int response = 1;
+        long long int factorial = 1;
+
         std::cout << "First number (enter 'q' to exit): ";
-        if (!(std::cin >> a)) {
+        if (!(std::cin >> a))
+        {
             std::cin.clear(); // Resetting error status
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Clearing the input buffer
             std::cout << "exiting" << std::endl;
             break;
         }
-
-        std::cout << "second number: ";
-        std::cin >> b;
-
-        std::cout << "Enter transaction (+, -, *, /): ";
+        std::cout << "Enter transaction (+, -, *, /,!,^): ";
         std::cin >> transaction;
 
-        switch (transaction) {
-        case '+':
-            std::cout << "response : " << a + b << std::endl;
-            break;
+        if (transaction == '!')
+        {
 
-        case '-':
-            std::cout << "response : " << a - b << std::endl;
+
+            if (a <= 20)
+            {
+
+
+                for (long int i = a; i >= 1; i--)
+                {
+                    factorial = factorial * i;
+                }
+                std::cout << factorial << std::endl;
+            }
+            else
+            {
+                std::cout << "error: unknown transactioan(20 at most! transaction is done.)\n";
+            }
+            
+            continue;
+            
+        }
+        else {
+
+            std::cout << "second number: ";
+            std::cin >> b;
+        }
+        
+       switch (transaction) {
+       case '+':
+         std::cout << "response : " << a + b << std::endl;
+            break;
+ 
+       case '-':
+         std::cout << "response : " << a - b << std::endl;
             break;
 
         case '*':
-            std::cout << "response : " << a * b << std::endl;
+          std::cout << "response : " << a * b << std::endl;
             break;
 
-        case '/':
-            if (b != 0) {
-                std::cout << "response : " << a / b << std::endl;
-            }
-            else {
-                std::cout << "error: division by zero!" << std::endl;
-            }
+       case '/':
+          if (b != 0) {
+            std::cout << "response : " << a / b << std::endl;
+          }
+          else {
+            std::cout << "error: division by zero!" << std::endl;
+          }
             break;
+       case'^':
 
-        default:
-            std::cout << "unknown transaction." << std::endl;
+        for (int i = 0; i < b; i++)
+        {
+            response = response * a;
         }
 
-        std::cout << std::endl;
-    }
+        std::cout << "sonuc:" << response << std::endl;
 
-    return 0;
+         break;
+
+        default:
+           std::cout << "unknown transaction." << std::endl;
+       
+       }
+
+      std::cout << std::endl;
+    }
+        return 0;
 }
